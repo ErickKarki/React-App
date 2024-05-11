de6bd1f5a238c5ad6bfeddd702d94a3cdffd5949
Erick Karki <ariksankarki381898@gmail.com>
2024-05-11 09:33:32 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Card from "./Card.jsx";
+function App() {
+  return <Card />;
+}
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import profilePic from "./assets/evansi.jpg";
+function Card() {
+  return (
+    <div className="card">
+      <img className="card-image" src={profilePic} alt="profile picture"></img>
+      <h2 className="card-title">Evansi Karki</h2>
+      <p>Gorakhkali English Boarding School</p>
+    </div>
+  );
+}
+
+export default Card;
