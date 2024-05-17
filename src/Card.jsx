0137a107d2594e278f0fe181e019b7f33913475d
Erick Karki <ariksankarki381898@gmail.com>
2024-05-17 07:47:58 +0545

    Erick

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ function Card() {
   return (
     <div className="card">
       <img className="card-image" src={profilePic} alt="profile picture"></img>
-      <h2 className="card-title">Evansi Karki</h2>
+      <h2 className="card-title">Evansi Karki-Sister of Erick Karki</h2>
       <p>Gorakhkali English Boarding School</p>
     </div>
   );
